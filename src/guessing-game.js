31559ec868a3dfe51e891ccf9461f9841e20bd81
Viktoriya Nykytenko <kitakiv@gmail.com>
2023-10-08 17:07:04 +0300

    feat: end the task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.guessnumber = Math.ceil((this.max + this.min) / 2)
+        return this.guessnumber;
     }
 
     lower() {
-
+        this.max = this.guessnumber;
     }
 
     greater() {
-
+        this.min = this.guessnumber;
     }
 }
 
